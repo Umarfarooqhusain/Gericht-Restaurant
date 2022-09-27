cb0d8f2cae6dffd153c9dc800d0060b6c9c069b8
Umarfarooqhusain <77962982+Umarfarooqhusain@users.noreply.github.com>
2022-09-27 14:56:54 +0530

    Gerichr Restaurant

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { images } from "../../constants";
+import "./AboutUs.css";
+
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          We all agree that food is life. We think we came up with something
+          kinda special and have the perfect casual environment to showcase our
+          creation.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Opened in 2019 in Uptown New York, GERICHT is the culmination of years
+          of thought and research by a handful of FOOD enthusiasts on a quest to
+          create the perfect kitchen.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">
+            Kevin’s appreciation of nature’s diversity and his endless
+          </p>
+        </div>
+        <p className="p__opensans">
+          experimentation in the kitchen and garden are the driving forces in
+          his cooking. Peter leads the Gericht team with a focus on excellence,
+          innovation and hospitality. So relax, kick back, have a slice of the
+          good life, and let us show you our take on your favorite dish.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
